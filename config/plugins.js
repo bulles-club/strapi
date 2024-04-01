4b471585557e48148d5ed20c310825940f4e699c
Benoit Quette <benoit.quette@gmail.com>
2024-04-01 09:41:19 +0200

    added User fields

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: [
+          "firstname",
+          "lastname",
+          "dateofbirth",
+          "phonenumber",
+          "addresses",
+        ],
+      },
+    },
+  },
+});
