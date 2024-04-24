c65bb2ff7fd12f2f754f32b22932f52a9c9dadd1
Benoit Quette <benoit.quette@gmail.com>
2024-04-24 18:28:36 +0200

    linting

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,38 +1,38 @@
-import type { Schema, Attribute } from '@strapi/strapi';
+import type { Schema, Attribute } from "@strapi/strapi";
 
 export interface BooksGenres extends Schema.Component {
-  collectionName: 'components_books_genres';
+  collectionName: "components_books_genres";
   info: {
-    displayName: 'Genres';
-    icon: 'apps';
-    description: '';
+    displayName: "Genres";
+    icon: "apps";
+    description: "";
   };
   attributes: {
-    genres: Attribute.Relation<'books.genres', 'oneToMany', 'api::genre.genre'>;
+    genres: Attribute.Relation<"books.genres", "oneToMany", "api::genre.genre">;
     title: Attribute.String;
   };
 }
 
 export interface BooksHeroSlider extends Schema.Component {
-  collectionName: 'components_books_hero_sliders';
+  collectionName: "components_books_hero_sliders";
   info: {
-    displayName: 'HeroSlider';
-    icon: 'stack';
+    displayName: "HeroSlider";
+    icon: "stack";
   };
   attributes: {
     books: Attribute.Relation<
-      'books.hero-slider',
-      'oneToMany',
-      'api::book.book'
+      "books.hero-slider",
+      "oneToMany",
+      "api::book.book"
     >;
   };
 }
 
-declare module '@strapi/types' {
+declare module "@strapi/types" {
   export module Shared {
     export interface Components {
-      'books.genres': BooksGenres;
-      'books.hero-slider': BooksHeroSlider;
+      "books.genres": BooksGenres;
+      "books.hero-slider": BooksHeroSlider;
     }
   }
 }
